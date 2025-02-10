59e722ca97faf8cb74b34aa8e0d8c1d5a55a74c0
unknown <thepancakeface823@gmail.com>
2025-02-09 21:55:22 -0300

    first commit

---
+++ backend/routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Inertia\Inertia;
+use Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware([
+    HandlePrecognitiveRequests::class, // Middleware padrão do Laravel 11
+    \Inertia\Middleware::class, // Middleware do Inertia.js
+])->group(function () {
+    Route::get('/home', function (Request $request) {
+        return Inertia::render('Home'); // Renderiza a página 'Home' no Vue
+    });
+});
